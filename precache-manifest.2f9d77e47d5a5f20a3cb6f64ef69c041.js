c445996549cd54f4a21aa672ae03927ddacacf63
WinZip <40386755+WinZip17@users.noreply.github.com>
2020-01-20 23:48:29 +0300

    Updates

--- precache-manifest.2f9d77e47d5a5f20a3cb6f64ef69c041.js
+++ precache-manifest.2f9d77e47d5a5f20a3cb6f64ef69c041.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "483cbfb99e25c5941e3e3fff26aace8d",
+    "revision": "61532049120df0286b56e1bfb563df33",
     "url": "/vk-app-traffic-check/index.html"
   },
   {
-    "revision": "16bd2ca0471f7c8b2568",
+    "revision": "9b275478d794d0478fa7",
     "url": "/vk-app-traffic-check/static/css/2.4e89ce74.chunk.css"
   },
   {
-    "revision": "d400b87cbfabd3a7caec",
-    "url": "/vk-app-traffic-check/static/css/main.98e7af26.chunk.css"
+    "revision": "8ce878b4424b90272aab",
+    "url": "/vk-app-traffic-check/static/css/main.116ed365.chunk.css"
   },
   {
-    "revision": "16bd2ca0471f7c8b2568",
-    "url": "/vk-app-traffic-check/static/js/2.e867a875.chunk.js"
+    "revision": "9b275478d794d0478fa7",
+    "url": "/vk-app-traffic-check/static/js/2.d843e5fc.chunk.js"
   },
   {
-    "revision": "d400b87cbfabd3a7caec",
-    "url": "/vk-app-traffic-check/static/js/main.26451c66.chunk.js"
+    "revision": "8ce878b4424b90272aab",
+    "url": "/vk-app-traffic-check/static/js/main.705165ae.chunk.js"
   },
   {
     "revision": "fc265d245dd105f67020",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk-app-traffic-check/precache-manifest.7da6de3af67a7c894f0b32409ec29676.js"
+  "/vk-app-traffic-check/precache-manifest.2f9d77e47d5a5f20a3cb6f64ef69c041.js"
 );
 
 self.addEventListener('message', (event) => {
