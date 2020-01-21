9858ff7e52beb8a9045640b5441fbe172bd84c43
WinZip <40386755+WinZip17@users.noreply.github.com>
2020-01-21 16:41:42 +0300

    Updates

--- precache-manifest.c8355f1115181d1692c3b9b21df50b98.js
+++ precache-manifest.c8355f1115181d1692c3b9b21df50b98.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b466c891c9ff4828576d035b865315a7",
+    "revision": "b3f427d8c8d05d636bd1b65d53e0933b",
     "url": "/vk-app-traffic-check/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vk-app-traffic-check/static/css/2.4e89ce74.chunk.css"
   },
   {
-    "revision": "d023b8329146a1e35338",
+    "revision": "9b8bb227274670185224",
     "url": "/vk-app-traffic-check/static/css/main.681d50a9.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vk-app-traffic-check/static/js/2.666c8e74.chunk.js"
   },
   {
-    "revision": "d023b8329146a1e35338",
-    "url": "/vk-app-traffic-check/static/js/main.a86e5316.chunk.js"
+    "revision": "9b8bb227274670185224",
+    "url": "/vk-app-traffic-check/static/js/main.069bcdde.chunk.js"
   },
   {
     "revision": "fc265d245dd105f67020",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk-app-traffic-check/precache-manifest.187feff9c77aa9aba7fe05849865a629.js"
+  "/vk-app-traffic-check/precache-manifest.c8355f1115181d1692c3b9b21df50b98.js"
 );
 
 self.addEventListener('message', (event) => {
