b8ac6aa9420fcc328d271f25ce26432b8a8b8c46
WinZip <wydo@protonmail.com>
2020-02-07 13:17:50 +0300

    Updates

---
+++ precache-manifest.fd2438cdd5939aacd0be7a03965c5ca9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d0fd69f462cf9c1c0a41fdefca7df4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "18bdd533b85f91bb58bc",
+    "url": "./static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "040bbeee2c547e310042",
+    "url": "./static/css/main.d2955ebd.chunk.css"
+  },
+  {
+    "revision": "18bdd533b85f91bb58bc",
+    "url": "./static/js/2.ec8c2c54.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.ec8c2c54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040bbeee2c547e310042",
+    "url": "./static/js/main.f3891e72.chunk.js"
+  },
+  {
+    "revision": "fd7f87a4aa5718e1b08d",
+    "url": "./static/js/runtime-main.0fbb9358.js"
+  },
+  {
+    "revision": "135d01ef0ef9f608ef63ea1ab9949b85",
+    "url": "./static/media/main.135d01ef.jpg"
+  },
+  {
+    "revision": "678046a086dc1892c19988431759cec8",
+    "url": "./static/media/no_image.678046a0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.671e179d264c96f4f00509cf8e074ff0.js"
+  "./precache-manifest.fd2438cdd5939aacd0be7a03965c5ca9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
